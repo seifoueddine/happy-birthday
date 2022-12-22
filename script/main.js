ff5fbfb5c05e586deadb92fd0148c15813f7cf93
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-12-22 21:24:07 +0100

    fix text

--- script/main.js
+++ script/main.js
@@ -53,8 +53,11 @@ const animationTimeline = () => {
   };
 
   const tl = new TimelineMax();
-  var audio = new Audio('img/h-b.mp3');
-  audio.play();
+  var x = document.getElementById("myAudio"); 
+
+  function playAudio() { 
+    x.play(); 
+  } 
   tl
     .to(".container", 0.1, {
       visibility: "visible"
